304963c2ebc3551a7b8ff24b40f55818601a77c1
amandine satin <amandine.satinlaplateforme.io>
2023-12-15 07:43:04 +0100

    pendu

---
+++ pendu.py
@@ -0,0 +1,122 @@
+import random
+
+def play_hangman():
+   
+    words = ["web", "pendu", "ordinateur", "intelligence", "artificiel", "fonction", "boucle", "plateforme", "interface", "graphique", "eminem", "cyber", "robot", "clavier", "souris", "cable"]
+
+    
+    word = random.choice(words)
+
+    
+    guessed_letters = []
+    tries = 7
+
+    
+    while True:
+       
+        display_word = ""
+        for letter in word:
+            if letter in guessed_letters:
+                display_word += letter
+            else:
+                display_word += "_"
+        print(display_word)
+
+        
+        if tries == 7:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print("_|___            ")
+        elif tries == 6:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |        (_)   ")
+            print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print(" |               ")
+            print("_|___            ")
+        elif tries == 5:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |        (_)   ")
+            print(" |         |     ")
+            print(" |         |     ")
+            print(" |               ")
+            print(" |               ")
+            print("_|___            ")
+        elif tries == 4:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |        (_)   ")
+            print(" |         |     ")
+            print(" |         |     ")
+            print(" |        /      ")
+            print(" |               ")
+            print("_|___            ")
+        elif tries == 3:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |        (_)   ")
+            print(" |         |     ")
+            print(" |         |     ")
+            print(" |        / \   ")
+            print(" |               ")
+            print("_|___            ")
+        elif tries == 2:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |        (_)   ")
+            print(" |        /|     ")
+            print(" |         |     ")
+            print(" |        / \   ")
+            print(" |               ")
+            print("_|___            ")
+        elif tries == 1:
+            print("  _________     ")
+            print(" |/        |    ")
+            print(" |        (_)   ")
+            print(" |        /|\    ")
+            print(" |         |     ")
+            print(" |        / \   ")
+            print(" |               ")
+            print("_|___            ")
+        else:
+            print("Désolé, vous avez perdu. Le mot était :", word)
+            break
+
+        
+        if "_" not in display_word:
+            print("Bravo, vous avez gagné !")
+            break
+
+        
+        letter = input("Entrez une lettre : ")
+
+        
+        if letter in guessed_letters:
+            print("Vous avez déjà proposé cette lettre. Veuillez en choisir une autre.")
+            continue
+
+        
+        guessed_letters.append(letter)
+
+        
+        if letter in word:
+            print("La lettre est dans le mot !")
+        else:
+            print("La lettre n'est pas dans le mot.")
+            tries -= 1
+
+        
+        if tries == 0:
+            print("Désolé, vous avez perdu. Le mot était :", word)
+            break
+
+
+play_hangman()
